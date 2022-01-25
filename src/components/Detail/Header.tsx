6ab6ac4610aed44d109c520919e87ff0349321e6
junghee.kim <wjdgml3834@naver.com>
2022-01-25 10:14:59 +0900

    ✨  Detail 페이지 header 구현

--- src/components/Detail/Header.tsx
+++ src/components/Detail/Header.tsx
@@ -1,3 +1,48 @@
+import styled from "@emotion/styled";
+import Link from "next/link";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <HeaderContainer>
+      <Section>
+        <Link href="/">
+          <a>
+            <ArrowBackIcon className="icon" />
+          </a>
+        </Link>
+        <Text>상품 상세</Text>
+        <Link href="/mylist">
+          <a>
+            <ShoppingBasketIcon className="icon" />
+          </a>
+        </Link>
+      </Section>
+    </HeaderContainer>
+  );
 };
+
+const HeaderContainer = styled.header`
+  width: 100%;
+  border-bottom: 1px solid #dbdbdb;
+  position: sticky;
+  top: 0;
+`;
+
+const Section = styled.section`
+  padding: 13px 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .icon {
+    color: #767676;
+    width: 28px;
+    height: 28px;
+  }
+`;
+
+const Text = styled.h1`
+  font-size: 18px;
+  font-weight: 500;
+`;
